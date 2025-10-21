3bae091b818a0617795e2bea3ccee8920cfdd1ed
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-10-20 20:55:42 -0700

    Add Email and EmailSendAttempt models, with EmailAttemptStatus, and EmailStatus enums.

--- mail.py
+++ mail.py
@@ -1,9 +1,14 @@
+from flask import Flask
+from jinja2 import Environment, FileSystemLoader
+from models import db
+from enum import StrEnum
+from accounts import Account, SubscriptionStatus
 import resend
 import os
 import secrets
 import string
-from jinja2 import Environment, FileSystemLoader
 import datetime
+
 
 def get_time() -> int:
     return int(datetime.datetime.now(datetime.UTC).timestamp())
@@ -39,8 +44,6 @@ class EmailVerificationAttempt:
         elif code == self.code and self.expiry < time:
             return EmailVerificationResult(False, None, "Code expired")
 
-
-
 class EmailManager:
     CODE_LENGTH = 9
     VERIFY_EXPIRY_MINS = 15
@@ -74,13 +77,117 @@ class EmailManager:
             return EmailVerificationResult(False, None, "Email not found.")
         return self.verification_attempts[email].verify(code)
 
-"""
-em = EmailManager()
-em.send_code("Shayan", "shayanbahrainy@gmail.com")
-result = em.verify("vink@aurorii.com", "JSkk222(((dj)))")
+class EmailAttemptStatus(StrEnum):
+    NOT_ATTEMPTED = "not_attempted"
+    SENT = "sent"
+    FAILED = "failed"
 
-if result.success:
-    print("fuck succedded")
-else:
-    print(result.reason)
-"""
+class EmailStatus(StrEnum):
+    OPEN = "open" #No emails have been queued yet
+    QUEUED = "attempts_queued" #Emails have been queued and will be sent out
+    CLOSED = "closed" #No open send attempts
+
+class EmailSendAttempt(db.Model):
+    __tablename__ = 'send_attempts'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    email_id = db.Column(db.Integer, db.ForeignKey('emails.id'), nullable=False)
+    email = db.relationship('Email', backref='send_attempts')
+
+    recipient_email = db.Column(db.String(50), db.ForeignKey('accounts.email'), nullable=False)
+    recipient = db.relationship('Account', backref='email_attempts')
+
+    status = db.Column(db.Enum(EmailAttemptStatus.NOT_ATTEMPTED.value, EmailAttemptStatus.SENT.value, EmailAttemptStatus.FAILED.value, name="EmailAttemptStatus"), nullable=False)
+    reattempt_time = db.Column(db.Integer)
+
+    def __init__(self, email_id: int, recipient_email: str, status: str):
+        self.email_id = email_id
+        self.recipient_email = recipient_email
+        self.status = status
+
+class EmailAudience(StrEnum):
+    ALL = "ALL"
+    PLUS = "PLUS"
+    FREE = "FREE"
+
+class Email(db.Model):
+    __tablename__ = 'emails'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.String(1000), nullable=False)
+    audience = db.Column(db.String(25), nullable=False)
+    status = db.Column(db.Enum(EmailStatus.OPEN.value, EmailStatus.QUEUED.value, EmailStatus.CLOSED.value, name="EmailStatus"), nullable=False)
+    opened_time = db.Column(db.Integer, nullable=False)
+    queued_time = db.Column(db.Integer)
+    closed_time = db.Column(db.Integer)
+
+    def __init__(self, title: str, body: str, audience: str):
+        self.title = title
+        self.body = body
+        self.audience = audience
+        self.status = EmailStatus.OPEN
+        self.opened_time = get_time()
+
+    @staticmethod
+    def __get_audience__(audience: EmailAudience=EmailAudience.ALL) -> list[str]:
+        if audience == EmailAudience.ALL:
+            return [account.email for account in db.session.query(Account).all()]
+        if audience == EmailAudience.PLUS:
+            return [account.email for account in db.session.query(Account).filter(Account.subscription_status == SubscriptionStatus.PLUS.value).all()]
+        if audience == EmailAudience.FREE:
+            return [account.email for account in db.session.query(Account).filter(Account.subscription_status == SubscriptionStatus.NONE.value).all()]
+
+    def send(self):
+        assert self.status == EmailStatus.OPEN
+        self.status = EmailStatus.QUEUED
+        audience_list = Email.__get_audience__(self.audience)
+        for email in audience_list:
+            db.session.add(EmailSendAttempt(self.id, email, EmailAttemptStatus.NOT_ATTEMPTED.value))
+        self.queued_time = get_time()
+        db.session.commit()
+
+    def __repr__(self):
+        return f"Email({self.title}, {self.body}, {self.status}, {self.opened_time})"
+
+if __name__ == "__main__":
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://scenery:Scenery@localhost:5432/scenery'
+
+    context = app.app_context()
+
+    db.init_app(app)
+
+    with context:
+        db.create_all()
+
+        all_email = Email("Here is a test email!", "And now... a test body", EmailAudience.ALL)
+        premium_email = Email("Here is a premium email!", "And now... a test body", EmailAudience.PLUS)
+        free_email = Email("Here is a free email!", "And now... a test body", EmailAudience.FREE)
+
+        db.session.add(all_email)
+        db.session.add(premium_email)
+        db.session.add(free_email)
+
+        db.session.commit()
+
+        input("Press enter to move to next phase...")
+
+        all_email.send()
+
+        input(all_email)
+
+        premium_email.send()
+
+
+        input(premium_email)
+
+        free_email.send()
+
+        print(free_email)
+
+
+
+
