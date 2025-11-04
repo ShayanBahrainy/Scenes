242966e94b5160d7f7f77ab5049b14b5ab1c2471
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-11-03 22:36:22 -0800

    Add backbutton to about me and faq page.

---
+++ static/faq.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", function () {
+    this.document.querySelector(".back-button").addEventListener("click", function () {
+        location = "/"
+    })
+})
